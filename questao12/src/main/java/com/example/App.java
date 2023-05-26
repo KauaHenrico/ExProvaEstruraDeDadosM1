5d0e2da76ed0643d08d6f17cbe22aa5cb1926638
KimiMitsuji <kauasouza.aluno@unipampa.edu.br>
2023-05-26 20:07:34 -0300

    att questão12

--- questao12/src/main/java/com/example/App.java
+++ questao12/src/main/java/com/example/App.java
@@ -1,22 +1,24 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
 public class App {
-    public static int calcularSoma(int m, int n) {
-        if (n == m) {
-            return m;
-        } else {
-            return m + calcularSoma(m + 1, n);
+        public static int calcularFatorial(int n) {
+            if (n == 0) {
+                return 1; /* Caso base: fatorial de 0 é igual a 1 */
+            } else {
+                return n * calcularFatorial(n - 1); /* Chamada recursiva: n! = n * (n - 1)! */
+            }
         }
-    }
-
-    public static void main(String[] args) {
-        int m = 1;
-        int n = 5;
-        int resultado = calcularSoma(m, n);
-        System.out.println("A soma dos números inteiros de " + m + " a " + n + " é: " + resultado);
-    }
+    
+        public static void main(String[] args) {
+            int n = 5;
+            int resultado = calcularFatorial(n);
+            System.out.println("O fatorial de " + n + " é: " + resultado);
+        }
+    /* Justificativa da escolha da implementação recursiva: A implementação recursiva é escolhida porque a definição matemática do fatorial é naturalmente recursiva, onde n! = n * (n-1)!.
+    A implementação recursiva reflete diretamente essa definição, tornando o código mais legível e compreensível. Para valores pequenos de n, a diferença de desempenho entre a implementação recursiva e a iterativa é insignificante. 
+    No entanto, em termos de eficiência, a implementação iterativa é geralmente mais eficiente para cálculos de fatorial. A implementação recursiva envolve chamadas de função adicionais e alocação de memória para manter a pilha de chamadas, o que pode ser custoso em termos de tempo de execução e uso de recursos. 
+    Em casos onde n é muito grande, a implementação recursiva pode ser menos eficiente e levar a um estouro de pilha (stack overflow). 
+    A escolha entre a implementação recursiva e a implementação iterativa depende do contexto do problema, das necessidades específicas e das restrições de eficiência. */
 }
+
+    
